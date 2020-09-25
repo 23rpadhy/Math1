62957e97ff6795e797de240a8db15dafb8a55740
Ronish <deepa.padhy@gmail.com>
2020-09-25 12:27:31 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 3;
+        System.out.println(y + x);
+       int z = 7;
+       int c = 49;
+      System.out.println(Math.pow(z, 2));
+      System.out.println(Math.sqrt(c));
+      int a = 2;
+      int b = 1;
+      System.out.println(Math.abs(a - b));
+      System.out.println(z/b);
+
+    }
+}
